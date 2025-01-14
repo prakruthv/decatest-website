ef3584ce5cdef3558c00c921bdbcb421925c51fe
prakruthv <prakruthvikram@gmail.com>
2025-01-14 17:17:38 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-function App() {
-    return <h1>Welcome to the DECA Test Website!</h1>;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
