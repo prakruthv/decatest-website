70673141834ffa1a74440fbe9bb5475cd37c7c85
prakruthv <prakruthvikram@gmail.com>
2025-01-14 18:32:42 -0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDG7V5L4jfRG9LYh_vqT7lsPYOBGVIsOqc",
@@ -10,9 +7,4 @@ const firebaseConfig = {
   messagingSenderId: "867976905039",
   appId: "1:867976905039:web:c447cd83096105b4ab853a"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export default firebaseConfig;
