c0f242b7a1e4399a628a789bdbb31d38ba0e69ae
prakruthv <prakruthvikram@gmail.com>
2025-01-16 23:38:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Quiz from "./Quiz";
 import Login from "./Login";
 import Progress from "./Progress";
+import Signup from "./Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/progress" element={<Progress />} />
       </Routes>
