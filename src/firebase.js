049f6946dc2f80d11ca473e63a251597a2030bc7
prakruthv <prakruthvikram@gmail.com>
2025-01-16 23:46:02 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,13 @@
+
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -8,11 +16,18 @@ const firebaseConfig = {
   projectId: "deca-test-website",
   storageBucket: "deca-test-website.appspot.com",
   messagingSenderId: "867976905039",
-  appId: "1:867976905039:web:c447cd83096105b4ab853a"
+  appId: "1:867976905039:web:c447cd83096105b4ab853a",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
 
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, db, doc, setDoc, getDoc };
+// Functions for Email/Password and Google Sign-In
+const emailSignUp = (email, password) => createUserWithEmailAndPassword(auth, email, password);
+const emailSignIn = (email, password) => signInWithEmailAndPassword(auth, email, password);
+const googleSignIn = () => signInWithPopup(auth, provider);
+const logout = () => signOut(auth);
+
+export { auth, db, doc, setDoc, getDoc, emailSignUp, emailSignIn, googleSignIn, logout };
